e7e23cd4f243b8fe97a959d848c5c032e127aa3f
kimjaehyuk <jhkim0097@gmail.com>
2024-01-26 19:05:08 +0900

    boot init

---
+++ backend/src/test/java/com/seoulJJ/hypertube/HypertubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoulJJ.hypertube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypertubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
