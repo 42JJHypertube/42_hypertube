f938e61c046ee2c2dff45251b2774695000cb2fa
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2024-01-29 23:47:06 +0900

    [feat] 기본 레이아웃, 페이지 컴포넌트 파일 작성 #3

--- frontend/src/app/(main)/account/layout.tsx
+++ frontend/src/app/(main)/account/layout.tsx
@@ -1,11 +1,11 @@
-// export default async function AccountPageLayout({
-//   userInfo,
-//   login,
-// }: {
-//   userInfo?: React.ReactNode
-//   login?: React.ReactNode
-// }) {
-//   const isLogined = true
+export default async function AccountPageLayout({
+  userInfo,
+  login,
+}: {
+  userInfo?: React.ReactNode
+  login?: React.ReactNode
+}) {
+  const isLogined = true
 
-//   return <div>isLogined ? userInfo : login</div>
-// }
+  return <div>{isLogined ? userInfo : login}</div>
+}
