e4cb28c0b62e19bfedde589cd077e822138b216c
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2024-01-29 20:10:24 +0900

    test

--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -1,7 +1,7 @@
 import styles from './main.module.scss'
 
 export default function Home() {
-  const foo = true;
+  const foo2 = true;
   return (
     <div className={styles.main}>
       <h1>Main</h1>
