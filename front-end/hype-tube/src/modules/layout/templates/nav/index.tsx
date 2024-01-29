2d21e476e682bb56a6648542ab4433c8e0ab1db6
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2024-01-29 19:37:17 +0900

    [Style] 스타일 수정

--- front-end/hype-tube/src/modules/layout/templates/nav/index.tsx
+++ front-end/hype-tube/src/modules/layout/templates/nav/index.tsx
@@ -9,6 +9,7 @@ export default function Nav() {
           <div className="menubar">menuBar</div>
           <div className="searchBar">serachBar</div>
           <div className="Account">Account</div>
+          <div className="Language">Language</div>
         </nav>
       </header>
     </div>
