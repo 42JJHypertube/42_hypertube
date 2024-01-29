68070f687fdd04166c7588ce60372f447879b157
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2024-01-29 21:31:33 +0900

    fix build folder, dockerfile

---
+++ frontend/src/app/(main)/account/layout.tsx
@@ -0,0 +1,11 @@
+// export default async function AccountPageLayout({
+//   userInfo,
+//   login,
+// }: {
+//   userInfo?: React.ReactNode
+//   login?: React.ReactNode
+// }) {
+//   const isLogined = true
+
+//   return <div>isLogined ? userInfo : login</div>
+// }
--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -3,7 +3,7 @@ import styles from './main.module.scss'
 export default function Home() {
   return (
     <div className={styles.main}>
-      <h1>Main</h1>
+      <h1>Main changed</h1>
     </div>
   )
 }
