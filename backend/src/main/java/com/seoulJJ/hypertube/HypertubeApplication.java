7964b1d8187f5792f9d029efea0dc2ab1e713d34
kimjaehyuk <jhkim0097@gmail.com>
2024-02-15 20:12:53 +0900

    [Feat] Cors 설정 추가 #4

--- backend/src/main/java/com/seoulJJ/hypertube/HypertubeApplication.java
+++ backend/src/main/java/com/seoulJJ/hypertube/HypertubeApplication.java
@@ -3,6 +3,10 @@ package com.seoulJJ.hypertube;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "https://localhost", description = "Local Server")})
 @SpringBootApplication
 public class HypertubeApplication {
 
