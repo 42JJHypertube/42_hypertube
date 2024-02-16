847b4855b88a89c7414f9d946b005d4ae99c9c10
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2024-02-16 22:26:34 +0900

    [fix] regist함수 return type error  수정#6

--- frontend/src/modules/account/action.ts
+++ frontend/src/modules/account/action.ts
@@ -80,7 +80,7 @@ export async function registUser(
       default:
         return {
           ...formInfo,
-          error_message: ret.response.data.message,
+          error_message: ret.response.data.message as string,
           code: null,
           token: null,
         }
