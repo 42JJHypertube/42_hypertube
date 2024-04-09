df3378983e2ce357fe58ef3f87ab8d162b162c96
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2024-04-09 21:52:08 +0900

    [fix] 검색이 없을 시 notFound #26

--- frontend/src/app/(main)/search/[query]/page.tsx
+++ frontend/src/app/(main)/search/[query]/page.tsx
@@ -1,5 +1,6 @@
 import { searchMovie } from "@/modules/layout/templates/nav/action"
 import MovieSection from "@/modules/main/components/movieSection"
+import { notFound } from "next/navigation"
 
 export default async function SearchResult({
   params,
@@ -7,6 +8,9 @@ export default async function SearchResult({
   params: { query: string }
 }) {
   const res = await searchMovie({query: params.query, pages: 1})
+
+  if (res.response.status !== 200)
+    return notFound()
 
   return (
     <>
