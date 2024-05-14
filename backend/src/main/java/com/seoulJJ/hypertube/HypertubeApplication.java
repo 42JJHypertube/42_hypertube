dfd04174a32b379df14e6712a6ed4da165d7dde4
kimjaehyuk <jhkim0097@gmail.com>
2024-05-14 15:14:07 +0900

    [Feat] nginx 영상 path 추가 #27

--- backend/src/main/java/com/seoulJJ/hypertube/HypertubeApplication.java
+++ backend/src/main/java/com/seoulJJ/hypertube/HypertubeApplication.java
@@ -3,9 +3,6 @@ package com.seoulJJ.hypertube;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.extern.log4j.Log4j2;
-
-@Log4j2
 @SpringBootApplication
 public class HypertubeApplication {
 
